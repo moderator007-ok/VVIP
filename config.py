824e31d4aab48647cfc74814b8d8f4f26eb1de4b
moderator007-ok <meizharryputtar@gmail.com>
2025-02-07 21:59:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = ""
-    API_ID = 27536109
-    API_HASH = "b84d7d4dfa33904d36b85e1ead16bd63"
-    ADMIN = '6428531614'.split(',')
+    BOT_TOKEN = "8091849947:AAHOxvf9zHAZlnCebT7boSPP5OmvW5XhUC0"
+    API_ID = 29754529
+    API_HASH = "dd54732e78650479ac4fb0e173fe4759"
+    ADMIN = '7521176146'.split(',')
     ADMIN_ID = [int(id) for id in ADMIN]
-    DB_URL = "mo:iLfRpsD0S0.t88ua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    DB_NAME = "WARRIOR_EXTRACTOR"
-    TXT_LOG = -1002447298131
-    AUTH_LOG = -1002447298131
-    HIT_LOG = -1002447298131
-    DRM_DUMP = -1002447298131
-    CHANNEL = -1002447298131
-    CH_URL = ""
-    OWNER = ""
+    DB_URL = "mongodb+srv://tmglcd:kI1UijMr2jJXyOXY@cluster0.unjzi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_NAME = "XORG"
+    TXT_LOG = -1002330261438
+    AUTH_LOG = -1002330261438
+    HIT_LOG = -1002330261438
+    DRM_DUMP = -1002330261438
+    CHANNEL = -1002330261438
+    CH_URL = "-1002330261438"
+    OWNER = "7521176146"
     THUMB_URL = "https://vault.pictures/media/images/b9/ad/12/b9ad123d95cd49465fcb954d.jpg" #Replace by with your Thumb URL
     HOST = "https://drm-api-five.vercel.app"
 
